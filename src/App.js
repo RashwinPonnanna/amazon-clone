ecd14e4c89d2a0ad55285043fdd4d19b5edf28ee
Ubuntu <ubuntu@ip-172-31-25-0.ap-south-2.compute.internal>
2026-06-07 17:55:58 +0000

    amazon clone app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+
+      <img
+        src="https://m.media-amazon.com/images/I/61lwJy4B8PL._SX3000_.jpg"
+        width="100%"
+        alt="banner"
+      />
+
+      <div style={{padding:'20px'}}>
+
+        <h1>Amazon Clone</h1>
+
+        <h2>
+          Acer Aspire 5 Slim Laptop
+        </h2>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          AMD Ryzen 3 3200U
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <h3>$364.99</h3>
+
+      </div>
+
     </div>
   );
 }
